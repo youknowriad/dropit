0bb2548cab32bb4aef1e17f52e5c56ecec01a1b4
Riad Benguella <benguella@gmail.com>
2018-04-17 18:37:37 +0100

    Adding a loading container

--- scripts/sidebar/components/photo-list/index.js
+++ scripts/sidebar/components/photo-list/index.js
@@ -1,10 +1,7 @@
 import { Component } from "@wordpress/element";
-import { IconButton } from "@wordpress/components";
-import { createBlock } from "@wordpress/blocks";
-import { withDispatch } from "@wordpress/data";
 
-import "./style.scss";
-import { getPhotos, downloadPhoto } from "../../api";
+import Photo from "../photo";
+import { getPhotos } from "../../api";
 
 class PhotoList extends Component {
   constructor() {
@@ -20,95 +17,12 @@ class PhotoList extends Component {
     });
   }
 
-  getPhotoCaption(photo) {
-    return [
-      "Photo by ",
-      <a
-        key="link"
-        href={`https://unsplash.com/@${
-          photo.user.username
-        }?utm_source=wpsplash&utm_medium=referral`}
-      >
-        ${photo.user.name}
-      </a>,
-      " on ",
-      <a
-        key="link2"
-        href="https://unsplash.com/?utm_source=wpsplash&utm_medium=referral"
-      >
-        Unsplash
-      </a>
-    ];
-  }
-
-  addPhoto(photo) {
-    return () => {
-      const block = createBlock("core/image", {
-        url: photo.urls.regular,
-        caption: this.getPhotoCaption(photo)
-      });
-      this.props.insertBlock(block);
-    };
-  }
-
-  uploadPhoto(photo) {
-    return () => {
-      const createMediaFromFile = file => {
-        // Create upload payload
-        const data = new window.FormData();
-        data.append("file", file, file.name || file.type.replace("/", "."));
-        return wp.apiRequest({
-          path: "/wp/v2/media",
-          data,
-          contentType: false,
-          processData: false,
-          method: "POST"
-        });
-      };
-
-      downloadPhoto(photo.links.download_location, photo.id, {
-        type: "image/jpeg"
-      }).then(file => {
-        createMediaFromFile(file).then(image => {
-          const block = createBlock("core/image", {
-            id: image.id,
-            url: image.source_url,
-            link: image.link,
-            caption: this.getPhotoCaption(photo)
-          });
-          this.props.insertBlock(block);
-        });
-      });
-    };
-  }
-
   render() {
     const { photos } = this.state;
     return (
-      <div>
-        {photos.map(photo => (
-          <div key={photo.id} className="splash-sidebar-photo-list__photo">
-            <img src={photo.urls.small} />
-
-            <div className="splash-sidebar-photo-list__photo-toolbar">
-              <IconButton
-                className="button"
-                icon="upload"
-                onClick={this.uploadPhoto(photo)}
-              />
-              <IconButton
-                isPrimary
-                icon="plus"
-                onClick={this.addPhoto(photo)}
-              />
-            </div>
-          </div>
-        ))}
-      </div>
+      <div>{photos.map(photo => <Photo key={photo.id} photo={photo} />)}</div>
     );
   }
 }
 
-export default withDispatch(dispatch => ({
-  insertBlock: dispatch("core/editor").insertBlock
-}))(PhotoList);
+export default PhotoList;
---
+++ scripts/sidebar/components/photo/index.js
@@ -0,0 +1,109 @@
+import { Component } from "@wordpress/element";
+import { IconButton, Spinner } from "@wordpress/components";
+import { createBlock } from "@wordpress/blocks";
+import { withDispatch } from "@wordpress/data";
+
+import "./style.scss";
+import { downloadPhoto } from "../../api";
+
+class PhotoList extends Component {
+  constructor() {
+    super(...arguments);
+    this.addPhoto = this.addPhoto.bind(this);
+    this.uploadPhoto = this.uploadPhoto.bind(this);
+    this.state = {
+      loading: false
+    };
+  }
+
+  getPhotoCaption(photo) {
+    return [
+      "Photo by ",
+      <a
+        key="link"
+        href={`https://unsplash.com/@${
+          photo.user.username
+        }?utm_source=wpsplash&utm_medium=referral`}
+      >
+        ${photo.user.name}
+      </a>,
+      " on ",
+      <a
+        key="link2"
+        href="https://unsplash.com/?utm_source=wpsplash&utm_medium=referral"
+      >
+        Unsplash
+      </a>
+    ];
+  }
+
+  addPhoto() {
+    const { photo } = this.props;
+    const block = createBlock("core/image", {
+      url: photo.urls.regular,
+      caption: this.getPhotoCaption(photo)
+    });
+    this.props.insertBlock(block);
+  }
+
+  uploadPhoto() {
+    const { photo } = this.props;
+    const createMediaFromFile = file => {
+      // Create upload payload
+      const data = new window.FormData();
+      data.append("file", file, file.name || file.type.replace("/", "."));
+      return wp.apiRequest({
+        path: "/wp/v2/media",
+        data,
+        contentType: false,
+        processData: false,
+        method: "POST"
+      });
+    };
+    this.setState({ loading: true });
+
+    downloadPhoto(photo.links.download_location, photo.id, {
+      type: "image/jpeg"
+    }).then(file => {
+      createMediaFromFile(file).then(image => {
+        const block = createBlock("core/image", {
+          id: image.id,
+          url: image.source_url,
+          link: image.link,
+          caption: this.getPhotoCaption(photo)
+        });
+        this.props.insertBlock(block);
+        this.setState({ loading: false });
+      });
+    });
+  }
+
+  render() {
+    const { photo } = this.props;
+    const { loading } = this.state;
+    return (
+      <div key={photo.id} className="splash-sidebar-photo">
+        {loading && (
+          <div className="splash-sidebar-photo__spinner-container">
+            <Spinner />
+          </div>
+        )}
+        <img src={photo.urls.small} />
+        {!loading && (
+          <div className="splash-sidebar-photo__toolbar">
+            <IconButton
+              className="button"
+              icon="upload"
+              onClick={this.uploadPhoto}
+            />
+            <IconButton isPrimary icon="plus" onClick={this.addPhoto} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default withDispatch(dispatch => ({
+  insertBlock: dispatch("core/editor").insertBlock
+}))(PhotoList);
