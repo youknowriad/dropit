96381af776eb65f8bcf3a238451893c752ac2058
Riad Benguella <benguella@gmail.com>
2018-04-17 18:22:33 +0100

    Adding images from the sidebar

--- scripts/sidebar/api/index.js
+++ scripts/sidebar/api/index.js
@@ -16,3 +16,28 @@ export const searchPhotos = (search, page = 1) =>
       `https://api.unsplash.com/search/photos/?client_id=${clientId}&per_page=20&page=${page}&query=${search}`
     )
     .then(toJson);
+
+export const downloadPhoto = (url, name, options) =>
+  window
+    .fetch(`${url}?client_id=${clientId}`)
+    .then(toJson)
+    .then(({ url }) => {
+      return new Promise(resolve => {
+        const img = new window.Image();
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        img.onload = function() {
+          const maxWidthHeight = 2000;
+          const ratio = this.naturalWidth / this.naturalHeight;
+          const width = ratio > 1 ? maxWidthHeight : maxWidthHeight * ratio;
+          const height = ratio < 1 ? maxWidthHeight : maxWidthHeight / ratio;
+          c.width = width;
+          c.height = height;
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width, height);
+          c.toBlob(resolve, "image/jpeg", 0.75);
+        };
+        img.crossOrigin = "";
+        img.src = url;
+      });
+    })
+    .then(blob => new window.File([blob], name + ".jpg", options));
--- scripts/sidebar/components/photo-list/index.js
+++ scripts/sidebar/components/photo-list/index.js
@@ -1,7 +1,10 @@
 import { Component } from "@wordpress/element";
+import { IconButton } from "@wordpress/components";
+import { createBlock } from "@wordpress/blocks";
+import { withDispatch } from "@wordpress/data";
 
 import "./style.scss";
-import { getPhotos } from "../../api";
+import { getPhotos, downloadPhoto } from "../../api";
 
 class PhotoList extends Component {
   constructor() {
@@ -10,23 +13,102 @@ class PhotoList extends Component {
       photos: []
     };
   }
+
   componentDidMount() {
     getPhotos().then(photos => {
       this.setState({ photos });
     });
   }
+
+  getPhotoCaption(photo) {
+    return [
+      "Photo by ",
+      <a
+        key="link"
+        href={`https://unsplash.com/@${
+          photo.user.username
+        }?utm_source=wpsplash&utm_medium=referral`}
+      >
+        ${photo.user.name}
+      </a>,
+      " on ",
+      <a
+        key="link2"
+        href="https://unsplash.com/?utm_source=wpsplash&utm_medium=referral"
+      >
+        Unsplash
+      </a>
+    ];
+  }
+
+  addPhoto(photo) {
+    return () => {
+      const block = createBlock("core/image", {
+        url: photo.urls.regular,
+        caption: this.getPhotoCaption(photo)
+      });
+      this.props.insertBlock(block);
+    };
+  }
+
+  uploadPhoto(photo) {
+    return () => {
+      const createMediaFromFile = file => {
+        // Create upload payload
+        const data = new window.FormData();
+        data.append("file", file, file.name || file.type.replace("/", "."));
+        return wp.apiRequest({
+          path: "/wp/v2/media",
+          data,
+          contentType: false,
+          processData: false,
+          method: "POST"
+        });
+      };
+
+      downloadPhoto(photo.links.download_location, photo.id, {
+        type: "image/jpeg"
+      }).then(file => {
+        createMediaFromFile(file).then(image => {
+          const block = createBlock("core/image", {
+            id: image.id,
+            url: image.source_url,
+            link: image.link,
+            caption: this.getPhotoCaption(photo)
+          });
+          this.props.insertBlock(block);
+        });
+      });
+    };
+  }
+
   render() {
     const { photos } = this.state;
     return (
       <div>
         {photos.map(photo => (
-          <a key={photo.id} className="splash-sidebar-photo-list__photo">
+          <div key={photo.id} className="splash-sidebar-photo-list__photo">
             <img src={photo.urls.small} />
-          </a>
+
+            <div className="splash-sidebar-photo-list__photo-toolbar">
+              <IconButton
+                className="button"
+                icon="upload"
+                onClick={this.uploadPhoto(photo)}
+              />
+              <IconButton
+                isPrimary
+                icon="plus"
+                onClick={this.addPhoto(photo)}
+              />
+            </div>
+          </div>
         ))}
       </div>
     );
   }
 }
 
-export default PhotoList;
+export default withDispatch(dispatch => ({
+  insertBlock: dispatch("core/editor").insertBlock
+}))(PhotoList);
